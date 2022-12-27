1fda34395da9f897d07de7de83a354ac9d2de813
alirezayoom <alirezayoom@gmail.com>
2022-12-27 22:20:03 +0430

    refactored - dried

--- 01/script.js
+++ 01/script.js
@@ -4,14 +4,17 @@ let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let highscore = 0;
 
+function displayMessage(message) {
+  document.querySelector(".numberChecker").textContent = message;
+}
+
 document.querySelector(".check").addEventListener("click", function () {
   const num = Number(document.querySelector(".numberInput").value);
 
   if (!num) {
-    document.querySelector(".numberChecker").textContent = "⛔ no number";
+    displayMessage("⛔ no number");
   } else if (num === secretNumber) {
-    document.querySelector(".numberChecker").textContent =
-      "🔥 that is correct ";
+    displayMessage("🔥 that is correct ");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".theNumber").textContent = secretNumber;
 
@@ -19,21 +22,13 @@ document.querySelector(".check").addEventListener("click", function () {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
-  } else if (num > secretNumber) {
+  } else if (num !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".numberChecker").textContent = "📈 too high";
+      displayMessage(num > secretNumber ? "📈 too high" : "📉 too low");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".numberChecker").textContent = "Loser";
-    }
-  } else if (num < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".numberChecker").textContent = "📉 too low";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".numberChecker").textContent = "Loser";
+      displayMessage("Loser");
     }
   }
 });
@@ -43,7 +38,7 @@ document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector(".theNumber").textContent = "?";
   document.querySelector(".numberInput").value = "";
-  document.querySelector(".numberChecker").textContent = "Start Guessing...";
+  displayMessage("Start Guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector("body").style.backgroundColor = "#222";
 });
