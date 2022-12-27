4bea5b959de6d833dd8029d626b2c968dbf5560e
alirezayoom <alirezayoom@gmail.com>
2022-12-27 23:57:04 +0430

    functionality added

--- 02/script.js
+++ 02/script.js
@@ -2,10 +2,20 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-
 const close = document.querySelector(".close");
 const btn = document.querySelectorAll(".btn");
 
-for (let i = 0; i < btn.length; i++) {
-  console.log(btn[i].textContent);
+function show() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
+function hide() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", show);
+}
+close.addEventListener("click", hide);
+overlay.addEventListener("click", hide);
