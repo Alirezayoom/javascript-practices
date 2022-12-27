c9c63c94c7debe617a1476b5596497c433d4a784
alirezayoom <alirezayoom@gmail.com>
2022-12-27 23:27:30 +0430

    modal window added

---
+++ 02/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const close = document.querySelector(".close");
+const btn = document.querySelectorAll(".btn");
+
+for (let i = 0; i < btn.length; i++) {
+  console.log(btn[i].textContent);
+}
