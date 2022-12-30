c0020a23fd3ea2245aec718c6e3cce077c0bd108
alirezayoom <alirezayoom@gmail.com>
2022-12-30 19:27:34 +0430

    toggle added

--- 03/script.js
+++ 03/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 //select elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 
@@ -18,7 +21,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functioanility
 btnRoll.addEventListener("click", function () {
@@ -32,7 +37,13 @@ btnRoll.addEventListener("click", function () {
   //3.check for rolled 1, swith to next player
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
