73df3bc9880e4b47f5db723d82d8d6373fe2e9fe
alirezayoom <alirezayoom@gmail.com>
2022-12-30 18:08:40 +0430

    functionality added

--- 03/script.js
+++ 03/script.js
@@ -3,6 +3,10 @@
 //select elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
+
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+
 const diceEl = document.querySelector(".dice");
 
 const btnNew = document.querySelector(".btn--new");
@@ -14,6 +18,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 //rolling dice functioanility
 btnRoll.addEventListener("click", function () {
   //1. generate a random dice roll
@@ -24,4 +30,9 @@ btnRoll.addEventListener("click", function () {
   diceEl.src = `dice-${dice}.png`;
 
   //3.check for rolled 1, swith to next player
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
 });
