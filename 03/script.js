26660cc4cde9c248e3f8f0f40b118c4956452673
alirezayoom <alirezayoom@gmail.com>
2022-12-31 07:37:08 +0430

    functionality added to new game btn

--- 03/script.js
+++ 03/script.js
@@ -25,14 +25,26 @@ function switchPlayer() {
 }
 
 //start conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score1El.textContent = 0;
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+
+  diceEl.classList.add("hidden");
+}
+
+init();
 
 //rolling dice functioanility
 btnRoll.addEventListener("click", function () {
@@ -78,3 +90,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
